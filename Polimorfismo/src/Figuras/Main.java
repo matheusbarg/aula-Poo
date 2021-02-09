986f3c3ff14ba192500fa3acaa53ce21cd12f97b
matheusbarg <48385549+matheusbarg@users.noreply.github.com>
2021-02-08 21:20:11 -0300

    commit

--- Polimorfismo/src/Figuras/Main.java
+++ Polimorfismo/src/Figuras/Main.java
@@ -8,9 +8,17 @@ public class Main {
 	public static void main(String[] args) {
 	
 	Retangulo rt = new Retangulo ("retangulo",20,50);
+	Circulo c = new Circulo ("Circulo",50);
+	Quadrado q = new Quadrado("Quadrado",50);
+	Triangulo t = new Triangulo("Triangulo",50,50,50);
 	
 	
-   System.out.println(rt.getArea());
+   System.out.println("Retangulo "+rt.getArea());
+   System.out.println("Circulo "+c.getArea());
+   System.out.println("Quadrado "+q.getArea());
+   System.out.println("Triangulo "+t.getArea());
+   
+   
 	
 	
 	}
